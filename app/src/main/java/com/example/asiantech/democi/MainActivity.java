6d5fd0268a1518406b1921182e56af042d5464e8
phuongdn <phuongdn@asiantech.vn>
2017-06-03 16:18:53 +0700

    Test first screen

--- app/src/main/java/com/example/asiantech/democi/MainActivity.java
+++ app/src/main/java/com/example/asiantech/democi/MainActivity.java
@@ -1,13 +1,37 @@
 package com.example.asiantech.democi;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    public Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(this);
+        Button btnRegister = (Button) findViewById(R.id.btnRegister);
+        btnRegister.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnLogin:
+                Log.d("xxx", "onClick: login");
+                break;
+            case R.id.btnRegister:
+                Log.d("xxx", "onClick: register");
+                break;
+            default:
+                new IllegalStateException("Not found id of view");
+                break;
+        }
     }
 }
